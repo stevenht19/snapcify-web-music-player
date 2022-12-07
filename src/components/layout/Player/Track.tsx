f27f67ccb13913898d2f76c182fa8546d3899cd0
stevenht19 <mstvdev19@gmail.com>
2022-12-06 20:14:53 -0500

    fix useToast hook import in track component

--- src/components/layout/Player/Track.tsx
+++ src/components/layout/Player/Track.tsx
@@ -1,4 +1,4 @@
-import { useToast } from './hooks/useToast'
+import { useToast } from './hooks'
 import { Song } from '@/models/Song'
 import { SongTrack as Track } from '@/components/atoms/Card/Song'
 import { Heart } from '@/components/atoms/Icon'
